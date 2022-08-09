2650e2595464ee331bcd3e7d60e1cfdc43d6ad92
syy <1834827009@qq.com>
2022-08-09 23:09:22 +0800

    修复版本不兼容bug

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
+import { useEffect, useState } from 'react'
 import '../styles/globals.css'
 import '../styles/index.scss'
 
+
 function MyApp ({ Component, pageProps }) {
+  const [showChild, setShowChild] = useState(false)
+  useEffect(() => {
+    setShowChild(true)
+  }, [])
+  if (!showChild) {
+    return null
+  }
   return <Component {...pageProps} />
 }
 
